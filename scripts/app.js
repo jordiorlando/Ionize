5a7504e017caddbd6fcba761e4f444d20f594721
Jordi Orlando <jordi.orlando@gmail.com>
2015-07-15 02:07:33 +0200

    Implement multiple selections and moving
    Still need to deal with re-sorting the array after every move. Also,
    need to check if group can move before moving.

--- scripts/app.js
+++ scripts/app.js
@@ -9,7 +9,7 @@
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('dom-change', function() {
-    console.log('Bindings have been resolved, content is loaded (remove this?)');
+    console.log('Bindings have been resolved, content is loaded');
 
     document.querySelector("paper-slider").addEventListener('immediate-value-change', function() {
       updateLayout();
@@ -20,6 +20,82 @@
   window.addEventListener('WebComponentsReady', function() {
     // imports are loaded and elements have been registered
   });
+
+  app.shiftKey = false;
+  app.controlKey = false;
+
+  window.addEventListener("keydown", function(event) {
+    if (event.defaultPrevented) {
+      return; // Should do nothing if the key event was already consumed.
+    }
+
+    switch (event.key) {
+      case "Shift":
+        app.shiftKey = true;
+        break;
+      case "Control":
+        app.controlKey = true;
+        break;
+      case "Alt":
+        app.controlKey = true;
+        break;
+      case "Meta":
+        app.controlKey = true;
+        break;
+      case "OS":
+        //app.osKey = true;
+        break;
+      case "Escape":
+        keyHTML.deselectAll();
+        break;
+      case "ArrowUp":
+        app.moveKey("up");
+        break;
+      case "ArrowDown":
+        app.moveKey("down");
+        break;
+      case "ArrowLeft":
+        app.moveKey("left");
+        break;
+      case "ArrowRight":
+        app.moveKey("right");
+        break;
+      default:
+        return; // Quit when this doesn't handle the key event.
+    }
+
+    // Consume the event for suppressing "double action".
+    event.preventDefault();
+  }, true);
+
+  window.addEventListener("keyup", function(event) {
+    if (event.defaultPrevented) {
+      return; // Should do nothing if the key event was already consumed.
+    }
+
+    switch (event.key) {
+      case "Shift":
+        app.shiftKey = false;
+        break;
+      case "Control":
+        app.controlKey = false;
+        break;
+      case "Alt":
+        app.controlKey = false;
+        break;
+      case "Meta":
+        app.controlKey = false;
+        break;
+      case "OS":
+        //app.osKey = false;
+        break;
+      default:
+        return; // Quit when this doesn't handle the key event.
+    }
+
+    // Consume the event for suppressing "double action".
+    event.preventDefault();
+  }, true);
 
   // Close drawer after menu item is selected if drawerPanel is narrow
   app.onMenuSelect = function() {
--- scripts/layout.js
+++ scripts/layout.js
@@ -1,7 +1,7 @@
 var app = document.querySelector('#app');
 
-app.unitSize = 100; // Size in px of 1U
-var color = "#DDD"; // Color for keys
+app.unitSize = 90; // Size in px of 1U
+app.stepSize = 0.25;
 var layout = {};
 
 app.saveLayout = function() {
@@ -50,6 +50,43 @@ app.addKey = function() {
   keyHTML.update(numKeys);
 };
 
+app.moveKey = function(dir) {
+  var container = document.querySelector("#layoutContainer");
+  var keys = document.querySelectorAll(".selected");
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys.item(i);
+    var k = parseInt(key.id.replace("key_", ""), 10);
+    key = layout.keys[k];
+
+    switch (dir) {
+      case "up":
+        key.y -= app.stepSize;
+        if (key.y < 0) {
+          key.y = 0;
+        }
+        keyHTML.update(k);
+        break;
+      case "down":
+        key.y += app.stepSize;
+        keyHTML.update(k);
+        break;
+      case "left":
+        key.x -= app.stepSize;
+        if (key.x < 0) {
+          key.x = 0;
+        }
+        keyHTML.update(k);
+        break;
+      case "right":
+        key.x += app.stepSize;
+        keyHTML.update(k);
+        break;
+    }
+
+    resizeLayout();
+  }
+};
+
 
 
 var serialize = function(obj) {
@@ -71,6 +108,7 @@ var renderLayout = function() {
   }
   container.style.width = (15 * app.unitSize) + "px";
   container.style.height = (5 * app.unitSize) + "px";
+  //container.addEventListener("click", function(){keyHTML.deselectAll();}, false);
 };
 
 var updateLayout = function() {
@@ -79,55 +117,50 @@ var updateLayout = function() {
   }
 };
 
-var keyHTML = function() {
+var resizeLayout = function() {
+  var container = document.querySelector("#layoutContainer");
 
+  container.style.width = (Math.max.apply(Math, layout.keys.map(function(key){return key.x + key.w;})) * app.unitSize) + "px";
+  container.style.height = (Math.max.apply(Math, layout.keys.map(function(key){return key.y + key.h;})) * app.unitSize) + "px";
 };
+
+var keyHTML = {};
 
 keyHTML.create = function(k) {
   var html = document.createElement("div");
   html.id = "key_" + k;
   html.className = "key";
-  html.style.position = "absolute";
-  html.style.display = "flex";
-  html.style.alignItems = "center";
-  html.style.justifyContent = "center";
-  //html.style.left = (key.x * app.unitSize) + "px";
-  //html.style.top = (key.y * app.unitSize) + "px";
   html.style.width = app.unitSize + "px";
   html.style.height = app.unitSize + "px";
 
   var keyBG = document.createElement("div");
   keyBG.className = "keyBG";
-  keyBG.style.display = "flex";
-  keyBG.style.alignItems = "center";
-  keyBG.style.justifyContent = "center";
   keyBG.style.width = (app.unitSize * 72.5 / 75) + "px";
   keyBG.style.height = (app.unitSize * 72.5 / 75) + "px";
-  keyBG.style.backgroundColor = color;
-  keyBG.style.border = "1px solid #888";
   keyBG.style.borderRadius = (app.unitSize / 10) + "px";
 
+  var keyMG = document.createElement("div");
+  keyMG.className = "keyMG";
+  keyMG.style.width = (app.unitSize * 55 / 75) + "px";
+  keyMG.style.height = (app.unitSize * 55 / 75) + "px";
+  keyMG.style.borderRadius = (app.unitSize / 10) + "px";
+
   var keyFG = document.createElement("div");
-  keyBG.style.display = "flex";
-  keyBG.style.alignItems = "center";
-  keyBG.style.justifyContent = "center";
+  keyFG.className = "keyFG";
   keyFG.style.width = (app.unitSize * 55 / 75) + "px";
   keyFG.style.height = (app.unitSize * 55 / 75) + "px";
-  keyFG.style.backgroundColor = "#FFF";
-  keyFG.style.border = "1px solid #BBB";
   keyFG.style.borderRadius = (app.unitSize / 10) + "px";
 
   var keyName = document.createElement("div");
-  //keyName.style.width = "100%";
-  //keyName.style.height = "100%";
   keyName.style.textAlign = "center";
   keyName.innerHTML = "NEW<br>" + k;
 
   keyFG.appendChild(keyName);
-  keyBG.appendChild(keyFG);
+  keyMG.appendChild(keyFG);
+  keyBG.appendChild(keyMG);
   html.appendChild(keyBG);
 
-  keyBG.addEventListener("click", function(){keyHTML.onClick(html.id)}, false);
+  keyBG.addEventListener("click", function(){keyHTML.onClick(k);}, false);
 
   return html;
 };
@@ -144,7 +177,11 @@ keyHTML.update = function(k) {
   keyBG.style.width = ((key.w * app.unitSize) - (app.unitSize * 2.5 / 75)) + "px";
   keyBG.style.height = ((key.h * app.unitSize) - (app.unitSize * 2.5 / 75)) + "px";
 
-  var keyFG = keyBG.firstElementChild;
+  var keyMG = keyBG.firstElementChild;
+  keyMG.style.width = ((key.w * app.unitSize) - (app.unitSize * 20 / 75)) + "px";
+  keyMG.style.height = ((key.h * app.unitSize) - (app.unitSize * 20 / 75)) + "px";
+
+  var keyFG = keyMG.firstElementChild;
   keyFG.style.width = ((key.w * app.unitSize) - (app.unitSize * 20 / 75)) + "px";
   keyFG.style.height = ((key.h * app.unitSize) - (app.unitSize * 20 / 75)) + "px";
 
@@ -153,15 +190,74 @@ keyHTML.update = function(k) {
 };
 
 keyHTML.onClick = function(id) {
-  var prevKey = document.querySelector(".selected");
-  if (prevKey !== null) {
-    prevKey.className = null;
-    prevKey.firstElementChild.style.border = "1px solid #888";
-  }
+  var key = document.querySelector("#key_" + id);
+  var lastKey = document.querySelector(".last");
 
-  var key = document.querySelector("#" + id);
-  key.className = "selected";
-  key.firstElementChild.style.border = "1px solid #0F0";
+  if (app.shiftKey) {
+    if (lastKey !== null) {
+      // Get index of previous key
+      var lastID = parseInt(lastKey.id.replace("key_", ""), 10);
+
+      // Remove .last from previous key
+      lastKey.className = lastKey.className.replace(" last", "");
+
+      // Select all the keys in between
+      if (lastID <= id) {
+        for (var i = lastID; i <= id; i++) {
+          var currKey = document.querySelector("#key_" + i);
+          if (currKey.className.indexOf("selected") == -1) {
+            currKey.className += " selected";
+          }
+        }
+      } else {
+        for (var i = lastID; i >= id; i--) {
+          var currKey = document.querySelector("#key_" + i);
+          if (currKey.className.indexOf("selected") == -1) {
+            currKey.className += " selected";
+          }
+        }
+      }
+    }
+
+    // Add .selected.last to the clicked key
+    if (key.className.indexOf("selected") == -1) {
+      key.className += " selected";
+    }
+    if (key.className.indexOf("last") == -1) {
+      key.className += " last";
+    }
+  } else if (app.controlKey) {
+    // Add .selected.last to the clicked key
+    if (key.className.indexOf("selected") == -1) {
+      key.className += " selected";
+      if (key.className.indexOf("last") == -1) {
+        key.className += " last";
+      }
+      if (lastKey !== null) {
+        // Remove .last from previous key
+        lastKey.className = lastKey.className.replace(" last", "");
+      }
+    } else {
+      key.className = key.className.replace(" last", "").replace(" selected", "");
+    }
+  } else {
+    keyHTML.deselectAll();
+
+    // Add .selected.last to the clicked key
+    if (key.className.indexOf("selected") == -1) {
+      key.className += " selected";
+    }
+    if (key.className.indexOf("last") == -1) {
+      key.className += " last";
+    }
+  }
+};
+
+keyHTML.deselectAll = function() {
+  var keys = document.querySelectorAll(".selected");
+  for (var i = 0; i < keys.length; i++) {
+    keys.item(i).className = keys.item(i).className.replace(" last", "").replace(" selected", "");
+  }
 };
 
 /*$(".key").hover(function() {
